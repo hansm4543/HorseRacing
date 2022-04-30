2a5ef000c29f8c860fd39b3eea9486c86027f0bc
Hans-Märten Liiu <hm4543@tlu.ee>
2022-04-30 17:54:25 +0300

    updated href to localhost

--- horserace-front-end/src/component/href.js
+++ horserace-front-end/src/component/href.js
@@ -1,4 +1,4 @@
-//const link ="http://localhost:5000/";
-const link ="https://backend-horseracing.herokuapp.com/";
+const link ="http://localhost:5000/";
+//const link ="https://backend-horseracing.herokuapp.com/";
 
 module.exports =  link ;
